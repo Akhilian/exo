08db1b1b1ac26b312461047966332f0ecc76218a
Adrien Saunier <adrien.saunier>
2023-12-16 22:28:23 +0100

    tech: Add some CORS configuration

--- api/app/main.py
+++ api/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 
 from adapter import import_csv
 from app.infrastructure.database import create_db_and_tables
@@ -6,6 +7,19 @@ from app.infrastructure.repository import passenger_repository
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/file")
 async def ingest_csv(file: UploadFile):
